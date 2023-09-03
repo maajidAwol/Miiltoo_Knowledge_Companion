655af3c7e9948e315653660c303cac6d17c7d692
Ararsa-Derese <ararsaderese6@gmail.com>
2023-09-03 15:57:22 +0300

    :construction: fixing

--- app.py
+++ app.py
@@ -54,38 +54,13 @@ def send():
     prompt = None
     prompt =request.json.get("prompt")
     PERSIST = False
-    # PERSIST = False
-    # query = None
-    #
-    # loader = TextLoader("books/books.txt", encoding="utf-8")
-    #     # loader = DirectoryLoader("data/")
-    # index = VectorstoreIndexCreator().from_loaders([loader])
-    #
-    # chain = ConversationalRetrievalChain.from_llm(
-    #     llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0),
-    #     retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
-    # )
-    #
-    # chat_history = []
-    #
-    # # message = request.json.get("message")
-    # # query = input("Prompt: ")
-    #
-    # result = chain({"question": prompt, "chat_history": chat_history})
-    # print(result)
-    #
-    # chat_history.append((query, result['answer']))
-    # query = None
-
-
-
     if PERSIST and os.path.exists("persist"):
         print("Reusing index...\n")
         vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
         index = VectorStoreIndexWrapper(vectorstore=vectorstore)
     else:
 
-        loader = TextLoader("books/books.txt", encoding="utf-8")  # Use this line if you only need data.txt
+        loader = TextLoader("books/biology.txt", encoding="utf-8")  # Use this line if you only need data.txt
         # loader = DirectoryLoader("data/")
         if PERSIST:
             index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory": "persist"}).from_loaders([loader])
