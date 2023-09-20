beb0f0ba8c5e98b1ceab9d8a536cbb42b2fec7f8
Abdulmajid Awol <maajidawol@gmail.com>
2023-09-20 13:00:57 +0300

    clean the code

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask,render_template
+import time
+
+from flask import Flask, render_template, session, jsonify
 from flask import Flask, render_template, request
 import os
 import sys
@@ -31,6 +33,52 @@ def ext(text):
       print("Found a JSON-like pattern, but it's not valid JSON.")
   else:
     print("No JSON-like pattern found in the text.")
+def chat_function(prompt,path):
+    loader = TextLoader(path, encoding="utf-8")  # Use this line if you only need data.txt
+
+    index = VectorstoreIndexCreator().from_loaders([loader])
+
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0),
+        retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+    )
+    # session.pop("hist", None)
+    if "bio" not in session:
+        session["bio"] = []
+
+    chat_history = session["bio"]
+    result = chain({"question": prompt, "chat_history": chat_history})
+
+    # Append the current conversation turn to the chat history in the session
+    chat_history.append((prompt, result['answer']))
+    session["bio"] = chat_history  # Update the chat history in the session
+
+    query = None
+    print(result)
+    print(session["bio"])
+    return result
+def quiz_function(prompt, path):
+
+    loader = TextLoader(path, encoding="utf-8")
+    # loader = DirectoryLoader("data/")
+    index = VectorstoreIndexCreator().from_loaders([loader])
+
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0),
+        retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+    )
+
+    query = prompt
+
+    result = chain({"question": prompt, "chat_history": ""})
+    print(result)
+    r = ext(result["answer"])
+
+    query = None
+    if r== "fail":
+        render_template("bio-g9.html")
+    else:
+        return r
 
 os.environ["OPENAI_API_KEY"]="sk-"
 
@@ -53,90 +101,56 @@ def grade():
     return render_template("grade.html")
 @app.route("/request", methods=["POST"])
 def send():
-    prompt = None
-    prompt =request.json.get("prompt")
-    PERSIST = False
-    if PERSIST and os.path.exists("persist"):
-        print("Reusing index...\n")
-        vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
-        index = VectorStoreIndexWrapper(vectorstore=vectorstore)
-    else:
+    prompt = request.json.get("prompt")
+    # path = "books/biology.txt"
+    # result = chat_function(prompt, path)
+    time.sleep(10)
+    if "bio" not in session:
+        session["bio"] = []
 
-        loader = TextLoader("books/biology.txt", encoding="utf-8")  # Use this line if you only need data.txt
-        # loader = DirectoryLoader("data/")
-        if PERSIST:
-            index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory": "persist"}).from_loaders([loader])
-        else:
-            index = VectorstoreIndexCreator().from_loaders([loader])
+    chat_history = session["bio"]
+    mock_text = "I am your dedicated study companion, here to empower you in your academic journey. My mission is to assist you in comprehending your course materials and ultimately, helping you achieve better grades. With a wealth of knowledge and insightful analysis at my disposal, I'll break down complex concepts into digestible pieces, provide summaries, answer your questions, and offer valuable insights. Whether it's literature, science, history, or any other subject, I'm here to be your study buddy."
 
-    chain = ConversationalRetrievalChain.from_llm(
-        llm=ChatOpenAI(temperature=0),
-        retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
-    )
+    result = {"answer": mock_text}  # Mocking the result
 
-    chat_history = []
-
-    result = chain({"question": prompt, "chat_history": chat_history})
-
+    # Append the current conversation turn to the chat history in the session
     chat_history.append((prompt, result['answer']))
+    session["bio"] = chat_history  # Update the chat history in the session
+
     query = None
     print(result)
-    return result
+    print(session["bio"])
+    return jsonify(result)
+
 @app.route("/requestH", methods=["POST"])
 def ChatWHistory():
-    prompt =request.json.get("prompt")
-    PERSIST = False
+    # prompt = request.json.get("prompt")
+    # path = "books/history.txt"
+    # result = chat_function(prompt, path)
+    time.sleep(10)
+    chat_history = session["bio"]
+    mock_text = "I am your dedicated study companion, here to empower you in your academic journey. My mission is to assist you in comprehending your course materials and ultimately, helping you achieve better grades. With a wealth of knowledge and insightful analysis at my disposal, I'll break down complex concepts into digestible pieces, provide summaries, answer your questions, and offer valuable insights. Whether it's literature, science, history, or any other subject, I'm here to be your study buddy."
+
+    result = {"answer": mock_text}  # Mocking the result
+
+    # Append the current conversation turn to the chat history in the session
+    chat_history.append(("kk", result['answer']))
+    session["bio"] = chat_history  # Update the chat history in the session
+
     query = None
-
-    loader = TextLoader("books/history.txt", encoding="utf-8")
-        # loader = DirectoryLoader("data/")
-    index = VectorstoreIndexCreator().from_loaders([loader])
-
-    chain = ConversationalRetrievalChain.from_llm(
-        llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0),
-        retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
-    )
-
-    chat_history = []
-
-    # message = request.json.get("message")
-    # query = input("Prompt: ")
-
-    result = chain({"question": prompt, "chat_history": chat_history})
     print(result)
-
-    chat_history.append((query, result['answer']))
-    query = None
-    return result
+    print(session["bio"])
+    return jsonify(result)
 @app.route("/quiz_request",methods=["POST"])
 def quiz_send():
     quiz_number = request.json.get('number')
     chapter = request.json.get('chapter')
     subtopic = request.json.get('subtopic')
     prompt = f'generate a {quiz_number} conceptual and random question quiz from  the content specially from {chapter}:characteristics and subtopic {subtopic} having four choices a,b,c,d and answer letter and explanation  of the answer in json format'
-    PERSIST = False
+    path = "books/biology.txt"
+    result = quiz_function(prompt, path)
+    return result
 
-
-    loader = TextLoader("books/books.txt", encoding="utf-8")
-    # loader = DirectoryLoader("data/")
-    index = VectorstoreIndexCreator().from_loaders([loader])
-
-    chain = ConversationalRetrievalChain.from_llm(
-        llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0),
-        retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
-    )
-
-    query = prompt
-
-    result = chain({"question": prompt, "chat_history": ""})
-    print(result)
-    r = ext(result["answer"])
-
-    query = None
-    if r== "fail":
-        render_template("bio-g9.html")
-    else:
-        return r
 
 @app.route("/quiz_requestH",methods=["POST"])
 def quizH_send():
@@ -144,28 +158,9 @@ def quizH_send():
     chapter = request.json.get('chapter')
     subtopic = request.json.get('subtopic')
     prompt = f'generate a {quiz_number} conceptual and random question quiz from  the content specially from {chapter}:characteristics and subtopic {subtopic} having four choices a,b,c,d and answer letter and explanation  of the answer in json format'
-    PERSIST = False
+    path = "books/history.txt"
+    result = quiz_function(prompt, path)
 
-
-    loader = TextLoader("books/history.txt", encoding="utf-8")
-    # loader = DirectoryLoader("data/")
-    index = VectorstoreIndexCreator().from_loaders([loader])
-
-    chain = ConversationalRetrievalChain.from_llm(
-        llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0),
-        retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
-    )
-
-    query = prompt
-
-    result = chain({"question": prompt, "chat_history": ""})
-    print(result)
-    r = ext(result["answer"])
-
-    query = None
-    if r == "fail":
-        render_template("hist-g9.html")
-    else:
-        return r
+    return result
 if __name__ == "__main__":
     app.run(debug=True)
