51fbf7685a68eaa1e9efeb43e9ed7fd77ca30f88
Abdulmajid Awol <maajidawol@gmail.com>
2023-10-12 05:40:29 +0300

    quiz content dynamic

--- static/js/bioquiz.js
+++ static/js/bioquiz.js
@@ -101,7 +101,22 @@ submitButton.addEventListener("click", function () {
 
 function showResults() {
   score = 0; // Reset the score before evaluating user answers
-  resultsContainer.innerHTML = ""; // Clear previous results
+  resultsContainer.innerHTML = `  <div class="result-label2-container">
+            <div class="result-line"></div>
+            <div class="result-label2">result</div>
+           </div>
+
+           <div class="explanation-label">
+            <span class="fether-icon">
+              <i class="fa-solid fa-feather"></i>
+            </span>
+            explanation
+          </div>
+                      <span class="chatbot-icon">
+              <img src="/static/asset/logo.svg" alt="">
+
+            </span>
+           `;
   resultsContainer.classList.add("expln-content");
 
   quizData.forEach((question, index) => {
@@ -122,9 +137,9 @@ function showResults() {
 
       resultElement.innerHTML = `
         <div class="question-container"><span class="quize-no"><strong>Question ${index + 1}:</strong></span><p class="question"> ${question.question}</p></div>
-       <div class="your-answer">Your answer: ${userChoice}</p>
-         <div class="correct-answer">Correct answer: ${correctAnswer}</p>
-        <p class="expln-detail">Explanation: ${explanation}</p>
+       <div class="your-answer">Your answer: ${userChoice}</div>
+         <div class="correct-answer">Correct answer: ${correctAnswer}</div>
+        <div class="expln-detail">Explanation: ${explanation}</div>
       `;
       resultsContainer.appendChild(resultElement);
     }
@@ -138,63 +153,63 @@ function showResults() {
              </div>
            </div>`;
 }
-var biology = {
-  "chapter 1:Introduction to Biology": {
-    "1.1:Definition of Biology ": [],
-    "1.2:Why do we study Biology?": [],
-    "1.3:The scientific method": [],
-    "1.4:Tools of a Biologist": [],
-    "1.5: Handling and using of light Microscope ": [],
-    "1.6:General Laboratory Safety Rules": [],
-  },
-  "chapter 2:Characteristics and Classification of organisms": {
-    "2.1: Characteristics of living things": [],
-    "2.2: Taxonomy of living things": [],
-    "2.3: Relevance of classification": [],
-    "2.4: Linnaean system of nomenclature": [],
-    "2.5: Common Ethiopian animals and plants": [],
-    "2.6: The five-kingdom system of classification": [],
-    "2.7: Renowned taxonomists in Ethiopia": [],
-  },
-  "chapter 3: Cells": {
-    "3.1: What is a cell?": [],
-    "3.2: Cell theory": [],
-    "3.3: Cell structure and function": [],
-    "3.4: Types of cells": [],
-    "3.5: Animal and plant cells": [],
-    "3.6: Observing cells under a microscope": [],
-    "3.7: The cell and its environment": [],
-    "3.8: Levels of Biological Organization": [],
-  },
-  "chapter 4 :Reproduction": {
-    "4.1: Introduction to reproduction": [],
-    "4.2: Asexual reproduction": [],
-    "4.3: Types of asexual reproduction ": [],
-    "4.4: Sexual reproduction": [],
-    "4.5: Primary and secondary sexual characteristics": [],
-    "4.6: Male reproductive structures": [],
-    "4.7: Female reproductive structures": [],
-    "4.8: The Menstrual cycle": [],
-    "4.9: Fertilization and pregnancy": [],
-    "4.10: Methods of birth control": [],
-    "4.11: Sexually transmitted infection: transmission and prevention": [],
-  },
-  "chapter 5:Human Health, Nutrition, and Disease ": {
-    "5.1: What is food?": [],
-    "5.2: Nutrition": [],
-    "5.3: Nutrients ": [],
-    "5.4: Balanced diet": [],
-    "5.5: Deficiency diseases": [],
-    "5.6: Malnutrition": [],
-    "5.7: Substance abuse ": [],
-    "5.8: Types of diseases": [],
-    "5.9: Renowned Nutritionists in Ethiopia": [],
-  },
-  "chapter 6:Ecology": {
-    "6.1: Ecology": [],
-    "6.2: Ecological relationships": [],
-  },
-};
+//var biology = {
+//  "chapter 1:Introduction to Biology": {
+//    "1.1:Definition of Biology ": [],
+//    "1.2:Why do we study Biology?": [],
+//    "1.3:The scientific method": [],
+//    "1.4:Tools of a Biologist": [],
+//    "1.5: Handling and using of light Microscope ": [],
+//    "1.6:General Laboratory Safety Rules": [],
+//  },
+//  "chapter 2:Characteristics and Classification of organisms": {
+//    "2.1: Characteristics of living things": [],
+//    "2.2: Taxonomy of living things": [],
+//    "2.3: Relevance of classification": [],
+//    "2.4: Linnaean system of nomenclature": [],
+//    "2.5: Common Ethiopian animals and plants": [],
+//    "2.6: The five-kingdom system of classification": [],
+//    "2.7: Renowned taxonomists in Ethiopia": [],
+//  },
+//  "chapter 3: Cells": {
+//    "3.1: What is a cell?": [],
+//    "3.2: Cell theory": [],
+//    "3.3: Cell structure and function": [],
+//    "3.4: Types of cells": [],
+//    "3.5: Animal and plant cells": [],
+//    "3.6: Observing cells under a microscope": [],
+//    "3.7: The cell and its environment": [],
+//    "3.8: Levels of Biological Organization": [],
+//  },
+//  "chapter 4 :Reproduction": {
+//    "4.1: Introduction to reproduction": [],
+//    "4.2: Asexual reproduction": [],
+//    "4.3: Types of asexual reproduction ": [],
+//    "4.4: Sexual reproduction": [],
+//    "4.5: Primary and secondary sexual characteristics": [],
+//    "4.6: Male reproductive structures": [],
+//    "4.7: Female reproductive structures": [],
+//    "4.8: The Menstrual cycle": [],
+//    "4.9: Fertilization and pregnancy": [],
+//    "4.10: Methods of birth control": [],
+//    "4.11: Sexually transmitted infection: transmission and prevention": [],
+//  },
+//  "chapter 5:Human Health, Nutrition, and Disease ": {
+//    "5.1: What is food?": [],
+//    "5.2: Nutrition": [],
+//    "5.3: Nutrients ": [],
+//    "5.4: Balanced diet": [],
+//    "5.5: Deficiency diseases": [],
+//    "5.6: Malnutrition": [],
+//    "5.7: Substance abuse ": [],
+//    "5.8: Types of diseases": [],
+//    "5.9: Renowned Nutritionists in Ethiopia": [],
+//  },
+//  "chapter 6:Ecology": {
+//    "6.1: Ecology": [],
+//    "6.2: Ecological relationships": [],
+//  },
+//};
 
 
 var ado = document.getElementById("book_choice");
