ae2dc52a800c3499d89aebffe8e52993f1d929d2
Natnael Meseret <natnaelmeseret5@gmail.com>
2023-10-15 10:32:13 -0700

    upload is added to header and fixed

--- static/js/header.js
+++ static/js/header.js
@@ -17,29 +17,28 @@ body.addEventListener("click", () => {
 for (let i = 0; i < home_link.length; i++) {
   home_link[i].addEventListener("mouseover", () => {
     if (i === 0) {
-      n = 78 * i;
+      n = 73 * i;
       currentPosition = 0;
       grade_menu.classList.add("hidden");
       profile_menu.classList.add("hidden");
       grade_btn_icon.style.transform = "rotate(0deg)";
       profile_drop_icon.style.transform = "rotate(0deg)";
     } else if (i === 1) {
-      n = 78 * i;
+      n = 73 * i;
       grade_menu.classList.remove("hidden");
       profile_menu.classList.add("hidden");
       grade_btn_icon.style.transform = "rotate(180deg)";
       profile_drop_icon.style.transform = "rotate(0deg)";
-
       currentPosition = 1;
     } else if (i === 2) {
-      n = 95 * i;
+      n = 90 * i;
       currentPosition = 0;
       grade_menu.classList.add("hidden");
       profile_menu.classList.add("hidden");
       grade_btn_icon.style.transform = "rotate(0deg)";
       profile_drop_icon.style.transform = "rotate(0deg)";
     } else {
-      n = 99 * i;
+      n = 94 * i;
       currentPosition = 0;
       grade_menu.classList.add("hidden");
       profile_menu.classList.add("hidden");
@@ -57,4 +56,25 @@ profile_group.addEventListener("mouseover", () => {
   grade_menu.classList.add("hidden");
   grade_btn_icon.style.transform = "rotate(0deg)";
   profile_drop_icon.style.transform = "rotate(180deg)";
+});
+const upload_btn = document.querySelector("#upload-btn1");
+const upload_popup = document.querySelector(".upload-popup");
+const close_btn = document.querySelector(".close-popup");
+const overlay = document.querySelectorAll(".overlay");
+
+upload_btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  upload_popup.classList.toggle("hidden");
+  overlay[1].classList.toggle("hidden");
+  document.querySelector(".body").style.overflowY = "hidden";
+});
+close_btn.addEventListener("click", () => {
+  upload_popup.classList.toggle("hidden");
+  overlay[1].classList.toggle("hidden");
+  document.querySelector(".body").style.overflowY = "auto";
+});
+overlay[1].addEventListener("click", () => {
+  upload_popup.classList.toggle("hidden");
+  overlay[1].classList.toggle("hidden");
+  document.querySelector(".body").style.overflowY = "auto";
 });
