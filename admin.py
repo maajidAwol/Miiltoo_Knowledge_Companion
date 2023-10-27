f8fce2c6478aa576344a554b2008d5c70b5ea9c9
Abdulmajid Awol <maajidawol@gmail.com>
2023-10-27 20:13:51 +0300

    trying something

--- admin.py
+++ admin.py
@@ -1,6 +1,6 @@
 from flask_admin import Admin, BaseView, expose
 from flask_admin.contrib.sqla import ModelView
-from user import User, db,Books, Contest  # Import your User class and db
+# from user import User, db,Books, Contest  # Import your User class and db
 
 admin = Admin(name='Admin Panel', template_mode='bootstrap3')
 
@@ -10,6 +10,6 @@ class MyView(BaseView):
         return self.render('admin/my_view.html')
 
 admin.add_view(MyView(name='My View', endpoint='myview'))
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Books, db.session))
-admin.add_view(ModelView(Contest, db.session))
+# admin.add_view(ModelView(User, db.session))
+# admin.add_view(ModelView(Books, db.session))
+# admin.add_view(ModelView(Contest, db.session))
