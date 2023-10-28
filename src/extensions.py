12cb20fdd2a2dd6b288a6335c3d9cccda22ce488
Ararsa-Derese <ararsaderese6@gmail.com>
2023-10-28 18:32:16 +0300

    :recycle: fix your device

--- src/extensions.py
+++ src/extensions.py
@@ -1,12 +1,23 @@
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_bcrypt import Bcrypt
+# from flask_mail import Mail
+# from flask_migrate import Migrate
+# from flask_admin import Admin
+# from flask_login import LoginManager
+# bcrypt = Bcrypt()
+# mail = Mail()
+# migrate = Migrate()
+# db = SQLAlchemy()
+# admin=Admin(name='Admin Panel', template_mode='bootstrap3')
+# login=LoginManager()
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_admin import Admin
-from flask_login import LoginManager
+
 bcrypt = Bcrypt()
 mail = Mail()
 migrate = Migrate()
 db = SQLAlchemy()
 admin=Admin(name='Admin Panel', template_mode='bootstrap3')
-login=LoginManager()
--- src/models/contest.py
+++ src/models/contest.py
@@ -1,7 +1,17 @@
-from flask_login import UserMixin
+# from flask_login import UserMixin
+# from ..extensions import db
+# from datetime import datetime
+# class Contest(db.Model, UserMixin):
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     subject =db.Column(db.String)
+#     contest_data = db.Column(db.String)  # Store JSON data as a string
+#     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+#     is_approved = db.Column(db.Boolean, default=False)
+#     active = db.Column(db.Boolean, default=False)
 from ..extensions import db
 from datetime import datetime
-class Contest(db.Model, UserMixin):
+class Contest(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     subject =db.Column(db.String)
