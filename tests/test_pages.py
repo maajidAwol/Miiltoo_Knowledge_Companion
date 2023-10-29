dcab7cd6a7a6d5395c17d40251ee98bd8838f014
Ararsa-Derese <ararsaderese6@gmail.com>
2023-10-29 22:07:04 +0300

    :test_tube:  test

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -1,11 +1,11 @@
 def test_homePage(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b'<title>Dashboard</title>' in response.data
+    assert b'<title>Home</title>' in response.data
 def test_login(client):
     response = client.get('/login')
     assert response.status_code == 200
-    assert b'<title>Login Page</title>' in response.data
+    assert b'<title>Login </title>' in response.data
 def test_register(client):
     response = client.get('/signup')
     assert response.status_code == 200
