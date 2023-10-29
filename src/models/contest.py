ea4a32bd851afbd3a1b7ed1afbf5d7021101cfd8
Ararsa-Derese <ararsaderese6@gmail.com>
2023-10-30 02:00:35 +0300

    :rocket: Deployed

--- src/models/contest.py
+++ src/models/contest.py
@@ -9,13 +9,3 @@ class Contest(db.Model, UserMixin):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     is_approved = db.Column(db.Boolean, default=False)
     active = db.Column(db.Boolean, default=False)
-# from ..extensions import db
-# from datetime import datetime
-# class Contest(db.Model):
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     subject =db.Column(db.String)
-#     contest_data = db.Column(db.String)  # Store JSON data as a string
-#     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-#     is_approved = db.Column(db.Boolean, default=False)
-#     active = db.Column(db.Boolean, default=False)
--- src/models/user.py
+++ src/models/user.py
@@ -14,17 +14,4 @@ class User(db.Model, UserMixin):
     country = db.Column(db.String(255))
     city = db.Column(db.String(50))
 
-# from ..extensions import db
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String(80), unique=True, nullable=False)
-#     password = db.Column(db.String(80), nullable=False)
-#     username = db.Column(db.String(120))
-#     profile_pic = db.Column(db.String(255))
-#     school_level = db.Column(db.String(50))
-#     age = db.Column(db.Integer)
-#     school_name = db.Column(db.String(100))
-#     verification_code = db.Column(db.String(4))
-#     verified = db.Column(db.Boolean)
-#     country = db.Column(db.String(255))
-#     city = db.Column(db.String(50))
+
